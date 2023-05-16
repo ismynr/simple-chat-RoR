28582da00daeb6b5fb831eef5315045b8faa84fc
ismynr <gekkai7is.my@gmail.com>
2023-05-16 21:50:12 +0700

    define model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :participants, dependent: :destroy
+  has_many :conversations, through: :participants
+  has_many :messages, dependent: :destroy
+
   # encrypt password
   has_secure_password
 end
